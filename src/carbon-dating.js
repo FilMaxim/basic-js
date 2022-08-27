c061d1de8a344fdd5212bb51ae5b809f470b5f5b
FilMaxim <fil.maxim13@gmail.com>
2022-08-27 19:21:20 +0300

    fix: create get season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,21 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  console.log(sampleActivity)
+  let age =Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
+  return typeof sampleActivity === "string" && sampleActivity >= 1 && sampleActivity < MODERN_ACTIVITY && sampleActivity.length > 0 ?  age : false
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  /*throw new NotImplementedError('Not implemented');*/
+if (date === undefined) {
+  return"Unable to determine the time of year!";
+}
+if (date[Symbol.toStringTag] === 'Date') {
+  throw new Error("Invalid date!");
+}
+
+  try {
+
+    const season = ["winter", "spring", "summer", "autumn"];
+    const month = date.getMonth();
+
+    if (month >= 2 && month <= 4) return season[1];
+    if (month >= 5 && month <= 7) return season[2];
+    if (month >= 8 && month <= 10) return season[3];
+    if (month === 0 || month === 11 || month === 1) return season[0];
+    if (Array. isArray (date)) {throw new Error("Invalid date!");}
+  } catch {
+
+    throw new Error("Invalid date!");
+
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
