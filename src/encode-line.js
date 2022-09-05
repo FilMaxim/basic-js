06032d658baba594315e4845e1d282df9c510a68
FilMaxim <fil.maxim13@gmail.com>
2022-09-05 11:44:32 +0300

    fix: create task encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let newStr = `${str} `;
+  let result = "";
+  let count = 1;
+  for (let i = 1; i < newStr.length; i++) {
+    if (newStr[i] === newStr[i - 1]) {
+      count++;
+    } else {
+      result += count === 1 ? newStr[i - 1] : count + newStr[i - 1];
+
+      count = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
